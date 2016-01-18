2af6b0f159eae668852d7f5639e4ccdeb379e4cd
Diana Parvanova <diana@parvanova.com>
2016-01-18 22:11:33 +0200

    hide title for node type = course

--- sites/all/themes/professional_theme/template.php
+++ sites/all/themes/professional_theme/template.php
@@ -107,6 +107,11 @@ function professional_theme_preprocess_node(&$variables) {
   if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
     $variables['classes_array'][] = 'node-full';
   }
+///customized
+  if($node->type == 'course'){
+	$variables['title'] = NULL; 
+  }
+///end customized
 }
 
 
